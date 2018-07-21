6ac79bc1916d804fd40369d625b96535e28a6a69
Homo Efficio <homo.efficio@gmail.com>
2018-07-22 08:59:31 +0900

    Initial Commit

---
+++ src/main/java/io/homo/efficio/cryptomall/CryptoMallApplication.java
@@ -0,0 +1,12 @@
+package io.homo.efficio.cryptomall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoMallApplication.class, args);
+    }
+}
