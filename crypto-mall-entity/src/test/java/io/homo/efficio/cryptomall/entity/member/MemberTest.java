b7a243d00fbd41a8e8dc4aef2d57e9bb3506c43c
Homo Efficio <homo.efficio@gmail.com>
2018-07-22 17:51:34 +0900

    Added test case for Member construction with optional infos

--- crypto-mall-entity/src/test/java/io/homo/efficio/cryptomall/entity/member/MemberTest.java
+++ crypto-mall-entity/src/test/java/io/homo/efficio/cryptomall/entity/member/MemberTest.java
@@ -12,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MemberTest {
 
     @Test
-    public void 멤버생성() {
+    public void 멤버생성_with_배송정보() {
         Member member = new Member.Required(1L, "김삼랑", "010-2222-3333")
                 .shippingInfo(new ShippingInfo("김삼랑",
                         "02-7777-8888",
@@ -22,5 +22,20 @@ public class MemberTest {
         assertThat(member.getName()).isEqualTo("김삼랑");
         assertThat(member.getStatus()).isEqualTo(Member.Status.ACTIVE);
         assertThat(member.getGrade()).isEqualTo(Member.Grade.NORMAL);
+    }
+
+    @Test
+    public void 멤버생성_with_배송정보_비활성_우량() {
+        Member member = new Member.Required(1L, "김삼랑", "010-2222-3333")
+                .shippingInfo(new ShippingInfo("김삼랑",
+                        "02-7777-8888",
+                        "서울 광진구 가즈아차산 777", ShippingInfo.Method.TACKBAE))
+                .status(Member.Status.INACTIVE)
+                .grade(Member.Grade.PREMIUM)
+                .build();
+
+        assertThat(member.getName()).isEqualTo("김삼랑");
+        assertThat(member.getStatus()).isEqualTo(Member.Status.INACTIVE);
+        assertThat(member.getGrade()).isEqualTo(Member.Grade.PREMIUM);
     }
 }
