fc74df33168e90f3d6fad89913364d1dbee73356
Homo Efficio <homo.efficio@gmail.com>
2018-07-22 09:06:22 +0900

    Added crypto-mall-entity subproject

---
+++ crypto-mall-entity/src/main/java/io/homo/efficio/cryptomall/entity/CryptoMallEntityApplication.java
@@ -0,0 +1,12 @@
+package io.homo.efficio.cryptomall.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoMallEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoMallEntityApplication.class, args);
+    }
+}
