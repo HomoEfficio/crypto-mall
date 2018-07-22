9e36397622964073b868ed11c95c541797a529c0
Homo Efficio <homo.efficio@gmail.com>
2018-07-22 10:42:55 +0900

    Added test for Order.calculateAmounts()

---
+++ crypto-mall-entity/src/test/java/io/homo/efficio/cryptomall/entity/order/OrderTest.java
@@ -0,0 +1,33 @@
+package io.homo.efficio.cryptomall.entity.order;
+
+import io.homo.efficio.cryptomall.entity.product.Product;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author homo.efficio@gmail.com
+ * Created on 2018-07-22.
+ */
+public class OrderTest {
+
+    @Test
+    public void 주문가격계산() {
+        final Product product01 = new Product(1L, "IOTA T-shirt type A", 20.00d);
+        final Product product02 = new Product(2L, "IOTA T-shirt type B", 20.00d);
+        final Product product03 = new Product(3L, "EOS Hood type C", 50.00d);
+
+        final List<OrderItem> orderItems = Arrays.asList(
+                new OrderItem(product01, 3),
+                new OrderItem(product02, 5),
+                new OrderItem(product03, 7)
+        );
+
+        final Order order = new Order(orderItems);
+
+        assertThat(order.calculateAmounts()).isEqualTo(510d);
+    }
+}
