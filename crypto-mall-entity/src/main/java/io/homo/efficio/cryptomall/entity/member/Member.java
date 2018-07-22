c6146d2c996489e12d96d6ba75f2ab9814b6e288
Homo Efficio <homo.efficio@gmail.com>
2018-07-22 16:23:40 +0900

    Added Member skeleton

---
+++ crypto-mall-entity/src/main/java/io/homo/efficio/cryptomall/entity/member/Member.java
@@ -0,0 +1,28 @@
+package io.homo.efficio.cryptomall.entity.member;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2018-07-22
+ */
+public class Member {
+
+    public void changeName() {
+
+    }
+
+    public void changePhoneNumber() {
+
+    }
+
+    public void changeShippingInfo() {
+
+    }
+
+    public void changeGrade() {
+
+    }
+
+    public void changeStatus() {
+        
+    }
+}
