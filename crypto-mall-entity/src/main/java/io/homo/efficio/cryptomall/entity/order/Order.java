793d90e79cb468e56bb5deab80ed30d1fe0b84d6
Homo Efficio <homo.efficio@gmail.com>
2018-07-22 10:01:27 +0900

    Added Order with behaviors only

---
+++ crypto-mall-entity/src/main/java/io/homo/efficio/cryptomall/entity/order/Order.java
@@ -0,0 +1,40 @@
+package io.homo.efficio.cryptomall.entity.order;
+
+/**
+ * @author homo.efficio@gmail.com
+ * Created on 2018-07-22.
+ */
+public class Order {
+
+    public Long calculateAmounts() {
+        return null;
+    }
+
+    public void changeOrderItem() {
+
+    }
+
+    public void changeShippingInfo() {
+
+    }
+
+    public void cancel() {
+
+    }
+
+    public void complete() {
+
+    }
+
+    public boolean isOrderItemChangeable() {
+        return false;
+    }
+
+    public boolean isShippingInfoChangeable() {
+        return false;
+    }
+
+    public boolean isCancellable() {
+        return false;
+    }
+}
