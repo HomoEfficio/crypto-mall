f6a210871e351ff8f9ca3cb28663c5bd1d2e7348
Homo Efficio <homo.efficio@gmail.com>
2018-07-26 01:18:18 +0900

    Added Cart and its behaviors without implementations

---
+++ crypto-mall-entity/src/main/java/io/homo/efficio/cryptomall/entity/member/Cart.java
@@ -0,0 +1,24 @@
+package io.homo.efficio.cryptomall.entity.member;
+
+/**
+ * @author homo.efficio@gmail.com
+ * Created on 2018-07-26.
+ */
+public class Cart {
+
+    public void addItem() {
+
+    }
+
+    public void removeItem() {
+
+    }
+
+    public void transferToOrder() {
+
+    }
+
+    public void clear() {
+
+    }
+}
