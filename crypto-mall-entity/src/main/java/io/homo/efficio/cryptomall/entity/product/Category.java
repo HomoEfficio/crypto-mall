61dd2427c60eaa34a558d9962f1f4d2d81f05132
Homo Efficio <homo.efficio@gmail.com>
2018-07-28 22:31:52 +0900

    Set Category as a JPA entity - Applied @Entity, @Table, @Id, @GeneratedValue, @Column on Category - Fixed previous error - But produces error like below:   - Caused by: org.hibernate.MappingException: Could not determine type for: java.util.List, at table: category, for columns: [org.hibernate.mapping.Column(products)]

--- crypto-mall-entity/src/main/java/io/homo/efficio/cryptomall/entity/product/Category.java
+++ crypto-mall-entity/src/main/java/io/homo/efficio/cryptomall/entity/product/Category.java
@@ -3,6 +3,7 @@ package io.homo.efficio.cryptomall.entity.product;
 import lombok.Getter;
 import lombok.NonNull;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,11 +11,16 @@ import java.util.List;
  * @author homo.efficio@gmail.com
  * created on 2018-07-22
  */
+@Entity
+@Table(name = "CATEGORY")
 @Getter
 public class Category {
 
     // TODO: @NonNull 적용
 
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
     private Long id;
 
     private String name;
