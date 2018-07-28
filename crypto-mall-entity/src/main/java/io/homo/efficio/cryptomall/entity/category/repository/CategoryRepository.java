b25fd3e6f954ed81ae0e6db675099a5abd4efe26
Homo Efficio <homo.efficio@gmail.com>
2018-07-29 00:50:22 +0900

    Added CategoryRepository interface

---
+++ crypto-mall-entity/src/main/java/io/homo/efficio/cryptomall/entity/category/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package io.homo.efficio.cryptomall.entity.category.repository;
+
+import io.homo.efficio.cryptomall.entity.category.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author homo.efficio@gmail.com
+ * created on 2018-07-29
+ */
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
