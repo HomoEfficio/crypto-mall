5364ec03486a6a992fe42c79c5caecf3892786c9
Homo Efficio <homo.efficio@gmail.com>
2018-07-29 15:32:25 +0900

    Added test case for MemberRepository.findByEmail(String) with compile error

--- crypto-mall-entity/src/test/java/io/homo/efficio/cryptomall/entity/member/repository/MemberRepositoryTest.java
+++ crypto-mall-entity/src/test/java/io/homo/efficio/cryptomall/entity/member/repository/MemberRepositoryTest.java
@@ -1,6 +1,8 @@
 package io.homo.efficio.cryptomall.entity.member.repository;
 
+import io.homo.efficio.cryptomall.entity.member.Member;
 import io.homo.efficio.cryptomall.entity.member.MemberRepository;
+import io.homo.efficio.cryptomall.entity.order.ShippingInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 /**
@@ -29,6 +32,14 @@ public class MemberRepositoryTest {
 
     @Test
     public void whenFindByEmail__thenReturnMember() {
-        fail();
+        Member member = new Member.Required("김삼랑", "abcd!@#$", "010-2222-3333")
+                .id(1L)
+                .shippingInfo(new ShippingInfo("김삼랑",
+                        "02-7777-8888",
+                        "서울 광진구 가즈아차산 777", ShippingInfo.Method.TACKBAE))
+                .build();
+        em.persist(member);
+
+        Member persistedMember = repository.findByEmail("abc@def.com");
     }
 }
