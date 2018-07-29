5d39cbe9112ed84bd9dce1cceef439e8f5e00bd7
Homo Efficio <homo.efficio@gmail.com>
2018-07-29 19:09:31 +0900

    Commented out id() to fix previous PersistenceObjectException - But produces error below: Caused by: org.hibernate.InstantiationException: No default constructor for entity:  : io.homo.efficio.cryptomall.entity.order.ShippingInfo

--- crypto-mall-entity/src/test/java/io/homo/efficio/cryptomall/entity/member/repository/MemberRepositoryTest.java
+++ crypto-mall-entity/src/test/java/io/homo/efficio/cryptomall/entity/member/repository/MemberRepositoryTest.java
@@ -33,7 +33,7 @@ public class MemberRepositoryTest {
     @Test
     public void whenFindByEmail__thenReturnMember() {
         Member member = new Member.Required("김삼랑", "abcdef@ghi.com", "abcd!@#$", "010-2222-3333")
-                .id(1L)
+//                .id(1L)
                 .shippingInfo(new ShippingInfo("김삼랑",
                         "02-7777-8888",
                         "서울 광진구 가즈아차산 777", ShippingInfo.Method.TACKBAE))
