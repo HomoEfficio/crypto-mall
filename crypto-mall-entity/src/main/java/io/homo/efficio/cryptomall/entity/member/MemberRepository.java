2f60f2fb07f3096dea23de557aff4726d886b7ac
Homo Efficio <homo.efficio@gmail.com>
2018-07-29 13:19:26 +0900

    Added MemberRepository - Fixed previous compile error - But produces below error: Caused by: java.lang.IllegalArgumentException: Not a managed type: class io.homo.efficio.cryptomall.entity.member.Member at org.hibernate.metamodel.internal.MetamodelImpl.managedType(MetamodelImpl.java:473)

---
+++ crypto-mall-entity/src/main/java/io/homo/efficio/cryptomall/entity/member/MemberRepository.java
@@ -0,0 +1,12 @@
+package io.homo.efficio.cryptomall.entity.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author homo.efficio@gmail.com
+ * Created on 2018-07-29.
+ */
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+
+}
--- crypto-mall-entity/src/test/java/io/homo/efficio/cryptomall/entity/member/repository/MemberRepositoryTest.java
+++ crypto-mall-entity/src/test/java/io/homo/efficio/cryptomall/entity/member/repository/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package io.homo.efficio.cryptomall.entity.member.repository;
 
+import io.homo.efficio.cryptomall.entity.member.MemberRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
