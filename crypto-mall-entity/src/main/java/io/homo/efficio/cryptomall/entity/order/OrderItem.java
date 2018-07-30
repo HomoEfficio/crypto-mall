1c8eae855da5f0d2e63a73c6b70111d4714aa85f
Homo Efficio <homo.efficio@gmail.com>
2018-07-30 23:42:24 +0900

    Applied @ManyToOne, @JoinColumn on product in OrderItem
    - Fixed previous error
    - But produces error below:
    Caused by: java.lang.IllegalArgumentException: This class [class io.homo.efficio.cryptomall.entity.member.Cart] does not define an IdClass

--- crypto-mall-entity/src/main/java/io/homo/efficio/cryptomall/entity/order/OrderItem.java
+++ crypto-mall-entity/src/main/java/io/homo/efficio/cryptomall/entity/order/OrderItem.java
@@ -19,6 +19,8 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "product_id")
     private Product product;
 
     private int quantity;
