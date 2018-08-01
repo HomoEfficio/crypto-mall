7bfcfb0367d371ca45b7957dd47de82c91d0378b
Homo Efficio <homo.efficio@gmail.com>
2018-08-01 22:47:37 +0900

    Added findByOrderer() to OrderRepository
    - Test passed
    - XXX of findByXXX method should be the name of the field, not the type

--- crypto-mall-entity/src/main/java/io/homo/efficio/cryptomall/entity/order/repository/OrderRepository.java
+++ crypto-mall-entity/src/main/java/io/homo/efficio/cryptomall/entity/order/repository/OrderRepository.java
@@ -1,11 +1,16 @@
 package io.homo.efficio.cryptomall.entity.order.repository;
 
+import io.homo.efficio.cryptomall.entity.member.Member;
 import io.homo.efficio.cryptomall.entity.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 /**
  * @author homo.efficio@gmail.com
  * created on 2018-07-31
  */
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Optional<Order> findByOrderer(Member orderer);
 }
